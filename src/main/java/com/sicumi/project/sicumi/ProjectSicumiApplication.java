299fdc7050322d21565f86b8056733aab0563387
andreayadi <andreayadi@outlook.com>
2022-06-03 17:20:23 +0700

    Initial Commit

---
+++ src/main/java/com/sicumi/project/sicumi/ProjectSicumiApplication.java
@@ -0,0 +1,13 @@
+package com.sicumi.project.sicumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSicumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSicumiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sicumi/project/sicumi/ProjectSicumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicumi.project.sicumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSicumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
